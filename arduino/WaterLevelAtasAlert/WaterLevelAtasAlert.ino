08d594a2f8abcc49f5aef7505a9a515c22d112ff
Inka Riyadi <inka.riyadi@gmail.com>
2022-05-19 23:43:24 +0700

    tambah timestamp di waterlevelatas

--- arduino/WaterLevelAtasAlert/WaterLevelAtasAlert.ino
+++ arduino/WaterLevelAtasAlert/WaterLevelAtasAlert.ino
@@ -1,12 +1,22 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#define NTP_OFFSET   60 * 60      // In seconds
+#define NTP_INTERVAL 60 * 1000    // In miliseconds
+#define NTP_ADDRESS  "europe.pool.ntp.org"
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
+
 // 1. WIFI CONFIG
-const char* SSID = "";
-const char* SSID_PASSWORD = "";
+const char* SSID = "Tselhome-2002D";
+const char* SSID_PASSWORD = "RatuTayaInka999";
 
 // 2. MQTT CLIENT CONFIG
 char CLIENT[]= "app-Esp32ElectricalFloodSafetySystem";
@@ -49,7 +59,21 @@ void setup() {
   setupWifi();
   client.setServer(BROKER_SERVER, BROKER_PORT);
   client.setCallback(mqttCallback);
+  timeClient.begin();
+  timeClient.setTimeOffset(25200);
   
+}
+
+char *time_stamp(){
+  char *timestamp = (char *)malloc(sizeof(char) * 16);
+  time_t ltime;
+  ltime=time(NULL);
+  struct tm *tm;
+  tm=localtime(&ltime);
+  
+  sprintf(timestamp,"%04d%02d%02d%02d%02d%02d", tm->tm_year+1900, tm->tm_mon, 
+      tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+  return timestamp;
 }
 
 void loop() {
@@ -59,7 +83,10 @@ void loop() {
   }
   client.loop();
 
+  timeClient.update();
   int waterLevelDetected = readWaterLevelSensor();
+  String formattedTime = timeClient.getFormattedTime();
+  
 
   state = !state;
   digitalWrite(ledPin, state);
@@ -86,8 +113,9 @@ void loop() {
 
   // Sent Water Level Data when Water Detected
   if(waterLevelDetected > 0){
-    sprintf(payload, "{\"water_level\": %d}", waterLevelDetected);
+    sprintf(payload, "{\"water_level\": %d, \"timestamp\": %s}", waterLevelDetected, formattedTime);
     Serial.println(payload);
+    
     client.publish(DEVICE, payload);
   }
 
